b3b4e899c6ebc622dfad4618681bdbff5382152d
Gaurav <theregaurav08@gmail.com>
2025-06-24 09:26:02 +0530

    added user authentication

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,17 @@ import {BrowserRouter} from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 
-// Import your Publishable Key
+// Imported Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
-
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  </ClerkProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
 )
