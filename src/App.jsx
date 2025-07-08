22bdc80eebbc3be2a45470467739cd64aca17033
Gaurav <theregaurav08@gmail.com>
2025-07-09 00:00:02 +0530

    added listmovies page

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,41 @@ import MovieDetails from './pages/MovieDetails'
 import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
+import Layout from './pages/admin/Layout'
+import Dashboards from './pages/admin/Dashboards'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 
 const App = () => {
-
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
 
   return (
     <>
-    <Toaster/>
-  {!isAdminRoute && <Navbar/>}
-  <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/movies' element={<Movies/>} />
-    <Route path='/movies/:id' element={<MovieDetails/>} />
-  <Route path='/movies/:id/:date' element={<SeatLayout/>} />
-  <Route path='/my-bookings' element={<MyBookings/>} />
-  <Route path='/favorite' element={<Favorite/>} />
-  </Routes>
-  {!isAdminRoute && <Footer/>}
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+
+      <Routes>
+        {/* Public routes */}
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+
+        {/* Admin nested routes */}
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboards />} />
+          <Route path='add-shows' element={<AddShows />} />
+          <Route path='list-shows' element={<ListShows />} />
+          <Route path='list-bookings' element={<ListBookings />} />
+        </Route>
+      </Routes>
+
+      {!isAdminRoute && <Footer />}
     </>
   )
 }
--- src/pages/admin/ListBookings.jsx
+++ src/pages/admin/ListBookings.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
+import { Loader } from 'lucide-react';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title';
 
 const ListBookings = () => {
-  return (
-    <div>ListBookings</div>
-  )
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const  [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getAllBookings = async () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false);
+  };
+
+  useEffect(()=>{
+    getAllBookings();
+  }, []);
+
+
+  return !isLoading ? (
+    <>
+      <Title text1="List" text2="Bookings"/>
+      <div className='max-w-4xl mt-6 overflow-x-auto'>
+
+      </div>
+    </>
+  ) : <Loading/>
 }
 
 export default ListBookings
